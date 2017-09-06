0cfff9d75a7a835434be2a07ce14de7a0b04ba2f
Ratnadeep Debnath <rtnpro@gmail.com>
2017-09-06 17:32:38 +0530

    Added a generic middleware to handle OPTIONS requests.

--- api/gin_engine_setup.go
+++ api/gin_engine_setup.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -13,12 +14,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// CORSMiddleware CORS middleware
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
+
 // NewGinEngine instanciates a new HTTP engine to server the requests
 func NewGinEngine(appDB *gormapplication.GormDB, notificationChannel notification.Channel, config *configuration.ConfigurationData) *gin.Engine {
 	httpEngine := gin.Default()
 	httpEngine.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
+	httpEngine.Use(CORSMiddleware())
 	authMiddleware := authz.NewJWTAuthMiddleware(appDB)
 	spacesResource := handler.NewSpacesResource(appDB, config, auth.NewAuthzResourceManager(config))
 	workitemsResource := handler.NewWorkItemsResource(appDB, notificationChannel, config)
